6b223fdbebdbde1f50c66eed91abf7c162ba84fe
Marwa Mubarak <marwamubarak6@gmail.com>
2022-12-28 16:07:30 +0200

    first commit

---
+++ src/test/java/com/software/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
