8b2a1af3aada51acc6f1b6b772b9128af321d9fc
Marwa Mubarak <marwamubarak6@gmail.com>
2022-12-28 17:27:54 +0200

    add response

---
+++ src/main/java/com/software/paymentservice/response/Response.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.software.paymentservice.response;
+
+
+public class Response {
+
+	private boolean status;
+	private String message;
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
